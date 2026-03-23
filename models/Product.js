dc16c62ffd2b1963a590dc159bd84847037f1edb
Ninoivo <tusuubira.ivan@yahoo.com>
2026-03-23 22:08:08 +0300

    Create Product model with schema for products

---
+++ models/Product.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    imageUrls: [{
+        type: String,
+        required: true
+    }],
+    stock: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+productSchema.pre('save', function(next) {
+    this.updatedAt = Date.now();
+    next();
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
