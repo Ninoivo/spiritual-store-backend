4be0488423d05ad3d7051a142a78acac55379cd8
Ninoivo <tusuubira.ivan@yahoo.com>
2026-03-23 22:07:30 +0300

    Create models/Cart.js file with cart schema

---
+++ models/Cart.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const cartSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
+    },
+    products: [{
+        productId: {
+            type: String,
+            required: true,
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            default: 1
+        }
+    }],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Cart', cartSchema);
